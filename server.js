2099585c4561766c40ff09b0732d27c3340f1161
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-22 18:15:28 -0700

    Fix app.listen so that server actually sends html file

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('port', process.env.PORT || 3000);
 
 app.locals.title = 'Palette Picker';
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, 'public'));
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} running on ${app.get('port')}`);
+})
