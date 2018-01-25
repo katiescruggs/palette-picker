23e2a4b7600acfe7ec9706f37dd62c440d6bfcb0
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-24 18:46:10 -0700

    Create handle post request in server for creating new projects

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const path = require('path');
+const app = express();
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+function checkRequirements(requestBody, requirements) {
+  for (let requiredParameter of requirements) {
+    if (!requestBody[requiredParameter]) {
+      return response.status(422).json({
+        error: `You are missing the required parameter ${requiredParameter}`
+      });
+    }
+  }
+}
 
 app.set('port', process.env.PORT || 3000);
 
-app.locals.title = 'Palette Picker';
-
-app.locals.projects = ['Rainbow', 'Christmas', 'StellaVia'];
-
-app.locals.palettes = {
-  Rainbow: [
-    ['#FF0000', '#FFFF00', '#00FF00', '#0000FF', '#FF00FF'],
-    ['#E01A4F', '#F15946', '#F9C22E', '#53B3CB', '#BF00FF'],
-    ['#00FF00', '#FF00FF', '#FF0000', '#FFFF00', '#0000FF']
-  ],
-  Christmas: [['#E02F49', '#06213F', '#184925', '#3A7C4C', '#F0F0F0']],
-  StellaVia: [['#000000', '#502F4C', '#666699', '#C8B8D8', '#F9F4F5']]
-};
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -25,12 +29,47 @@ app.get('/', (request, response) => {
 });
 
 app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} running on ${app.get('port')}`);
+  console.log(`Palette Picker running on ${app.get('port')}`);
 });
 
 app.get('/api/v1/projects', (request, response) => {
-  const projects = app.locals.projects;
-  return response.status(201).json({ projects });
+  database('projects').select()
+    .then(projects => {
+      return response.status(200).json({ projects });
+    })
+    .catch(error => {
+      return response.status(500).json({ error });
+    })
+});
+
+app.post('/api/v1/projects', (request, response) => {
+  const project = request.body;
+
+  for (let requiredParameter of ['title']) {
+    if (!project[requiredParameter]) {
+      return response.status(422).json({
+        error: `You are missing the required parameter ${requiredParameter}`
+      });
+    }
+  }
+
+  database('projects').insert(project, 'id')
+    .then(projectId => {
+      return response.status(201).json({ id: projectId[0] });
+    })
+    .catch(error => {
+      return response.status(500).json({ error });
+    })
+});
+
+app.get('/api/v1/palettes', (request, response) => {
+  database('palettes').select()
+    .then(palettes => {
+      return response.status(200).json({ palettes });
+    })
+    .catch(error => {
+      return response.status(500).json({ error });
+    })
 });
 
 app.get('/api/v1/palettes/:project', (request, response) => {
